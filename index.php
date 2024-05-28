7779bca453c160f4cd909cdcdd360ba4098bdf4e
Anuj kant Bhatt <88606783+anuj12345567899@users.noreply.github.com>
2024-05-28 20:51:01 +0530

    index.php
    index file php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/nav.php'); ?>
+
+<h1>Welcome to the College Event Management System</h1>
+<p>This is the landing page.</p>
+
+<?php include('includes/footer.php'); ?>
